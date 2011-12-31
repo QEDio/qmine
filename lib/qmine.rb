fb12d039172e95b64ffdf714c303ec5dcca53e55
Johannes Kaefer <jak4@lieserl.(none)>
2011-12-31 17:46:41 +0100

    split code in different routines prepare for external data

--- lib/qmine.rb
+++ lib/qmine.rb
@@ -4,6 +4,8 @@ require 'qmine/mine/mine.jar'
 
 module Qmine
   class Wrapper
+    attr_accessor :dataset, :options
+
     include_class 'main.BriefResult'
     include_class 'analysis.Analysis'
     include_class 'data.Dataset'
@@ -11,36 +13,64 @@ module Qmine
     include_class 'java.io.BufferedWriter'
     include_class 'main.Analyze'
     
+    def self.get_dataset( ext_options = {} )
+      options   = default_get_dataset_options.merge( ext_options.delete_if{ |k,v| v.nil? })
+      dataset   = nil
+
+
+      if( options[:inputfile] )
+        debug_stream    = FileWriter.new( options[:inputfile] + "_debug" )
+        debug_out       = BufferedWriter.new( debug_stream )
+        dataset         = Dataset.new( options[:inputfile], options[:debuglevel], debug_out)
+      end
+
+
+      return dataset
+    end
+
+    def self.default_get_dataset_options
+      {
+        :inputfile                        => 'spec/fixtures/WHO.csv',
+        :debuglevel                       => 0
+      }
+    end
+
+    def self.print( results, output )
+      Analyze.printResults(results, output, '1')
+    end
+
     def initialize( ext_options = {} )
       options         = default_options.merge( ext_options.delete_if{ |k,v| v.nil? })
-      
-      debug_stream    = FileWriter.new( options[:inputfile] + "_debug" )
-      debug_out       = BufferedWriter.new( debug_stream )
-      dataset         = Dataset.new( options[:inputfile], options[:debuglevel], debug_out)
-      a               = Analysis.new(dataset,Analysis::AnalysisStyle.allPairs)
-      
-      results          = a.getSortedResults(
-                            BriefResult,
-                            options[:inputfile],
-                            options[:required_common_vals_percent],
-                            options[:max_num_boxes_exponent],
-                            options[:num_clumps_factor],
-                            options[:gc_wait],
-                            '1',
-                            options[:debuglevel],
-                            debug_out
-                          )
-                          
-      Analyze.printResults(results, options[:inputfile], '1')
+
+      @dataset        = options.delete(:dataset)
+      @options        = options
     end
     
-    def buh
+    def analyze( style = 'allPairs' )
+      analysis          = Analysis.new(@dataset, Analysis::AnalysisStyle.allPairs)
+      results           = analysis.getSortedResults(
+                            BriefResult,
+                            @options[:id],
+                            @options[:required_common_vals_percent],
+                            @options[:max_num_boxes_exponent],
+                            @options[:num_clumps_factor],
+                            @options[:gc_wait],
+                            '1',
+                            @options[:debuglevel],
+                            @debug_out
+                          )
+
+      return results
     end
+
+
+
     
     def default_options
       {
+        :dataset                          => nil,
+        :id                               => "uuid",
         :debuglevel                       => 0,
-        :inputfile                        => 'spec/fixtures/WHO.csv',
         :gc_wait                          => 2147483647,
         :num_clumps_factor                => 15.0,
         :required_common_vals_percent     => 0.0,
--- spec/unit/wrapper_spec.rb
+++ spec/unit/wrapper_spec.rb
@@ -5,7 +5,9 @@ describe Qmine::Wrapper do
     let(:wrapper) { Qmine::Wrapper.new }
     
     it "works" do
-      wrapper.buh
+      wrapper.dataset = Qmine::Wrapper.get_dataset
+      results = wrapper.analyze
+      Qmine::Wrapper.print(results, wrapper.options[:inputfile])
     end
   end
 end
